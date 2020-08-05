35cfe9620c5d6edd6d16285de4e00aa735e6d31e
Your Name <you@example.com>
2020-08-05 14:17:29 -0400

    validate y stats

--- src/path.js
+++ src/path.js
@@ -1,13 +1,14 @@
 const path = require ("path");//Contiene métodos para trabajar con rutas
 
-/* if (path.extname(file) === '.md') {
-   getMd(file)
-    .then((fileData) => {
-      console.log(fileData);
-    })
-    .catch((error) => {
-      console.error(error)
-    })
+
+if (path.extname(file) === '.md') {
+  getMd(file)
+   .then((fileData) => {
+     console.log(fileData);
+   })
+   .catch((error) => {
+     console.error(error)
+   })
 } else {
-    console.log('Introduce una ruta que contenga un archivo con extención .md')
-} */
+   console.log('Introduce una ruta que contenga un archivo con extención .md')
+}
